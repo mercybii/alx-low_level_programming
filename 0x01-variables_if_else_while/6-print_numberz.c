6d598ee509822e8337fa043fad77314a4896a3c4
MercyBii <mercybii95@gmail.com>
2023-02-18 05:21:41 -0800

    print _numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,16 +1,19 @@
-#include  <stdio.h>
+#include <stdio.h>
+
 /**
- * all single digit numbers of base 10 starting from 0
- *followed by a new line.
+ * main - Entry single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0 (success)
  */
-int man (void)
+
+int main(void)
 {
-	inct c = "0";
-	 while (c < "9");
-	 {
-		 putchar(48 + c);
-		 c++;
-	 }
-	 putchar('\n');
-	 	 return (0);
+	int i;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+
+	putchar('\n');
+
+	return(0);
 }
