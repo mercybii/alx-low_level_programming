995bfe20080ec16db08fc59902364cef0d625e25
MercyBii <mercybii95@gmail.com>
2023-02-19 13:02:40 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,3 +1,11 @@
+#include <stdio.h>
+
+/**
+ *main- Entry point
+ *
+ *Return: Always 0 (success)
+ */
+
 int main(void)
 {
 	int digit1, digit2, digit3;
@@ -8,11 +16,11 @@ int main(void)
 		{
 			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
 			{
-		 		putchar((digit1 % 10) + '0');
-				putchar((digit2 % 10) + '0');
-				putchar((digit3 % 10) + '0');
+				putchar(digit1 + '0');
+				putchar(digit2 + '0');
+				putchar(digit3 + '0');
 
-				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+				if (digit1 == 7 || digit2 == 8 || digit3 == 9)
 					continue;
 
 				putchar(',');
