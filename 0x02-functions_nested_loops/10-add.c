2b7e1700821836ebad2d6ee4ddf881c20ebfdf52
MercyBii <mercybii95@gmail.com>
2023-02-22 06:45:21 -0800

    fghj

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,14 +1,17 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- *add - add two value
- *@n1: first number
- *@n2: second number
+ *main- check code
+ *
  *Return: Always 0
  */
 
-int add(int n1, int n2)
-
+int main(void)
 {
-	return (n1 - n2);
+	int n;
+
+	n = add(89, 9);
+	printf("%d\n", n);
+	return (0);
 }
