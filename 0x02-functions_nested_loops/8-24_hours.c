9b8bcd78516782cd113c65a395e2a9ec0deb4477
MercyBii <mercybii95@gmail.com>
2023-02-22 02:13:26 -0800

    8-24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
- * prints every minute of the day of Jack Bauer
- *
- * starting from 00:00 to 23:59.
+ *main- Entry point
+ *decription: print putchar
+ *Return: Always 0 (Sucess)
  */
+
 void jack_bauer(void)
 {
 	int a, b, c, d;
+
 	for (a = 0; a <= 2; a++)
 	{
 		for (b = 0; b <= 9; b++)
