d99bcbf15aaf6933368653396270868f5d1dc7d7
MercyBii <mercybii95@gmail.com>
2023-02-23 14:02:07 -0800

    ghjkl

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - Entry point
+ * Description: Prints 10 times 0 - 14
+ * Return: void
+ */
+void more_number(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+{
+	for (j = 0; j <= 14; j++)
+	{
+		if (j >= 10)
+		{
+			_putchar((j / 10) + '0');
+		}
+		_putchar((j % 10) + '0');
+	}
+}
+
