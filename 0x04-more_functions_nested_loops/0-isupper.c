6358d85fc62a3052f752ae86ab4da5d38a185509
MercyBii <mercybii95@gmail.com>
2023-02-23 09:07:57 -0800

    hgkj

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _isupper - Entry point
+ * Description: checks for uppercase characters
+ * @c: carrier integer variable
+ * Return: 1 on success, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	char uppercase = "A";
+	int  isupper = 0;
+
+	for (; uppercase <= 'Z'; uppercase++)
+	{
+		if (c == upercase)
+		{
+			isupper = i;
+			break;
+		}
+	}
+	return (isupper);
+}
+
