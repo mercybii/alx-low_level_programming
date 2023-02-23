25de89971d88f7bb13210df9c3b77fe38d4a2d79
MercyBii <mercybii95@gmail.com>
2023-02-23 11:25:14 -0800

    hjhjjf

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,25 +1,18 @@
 #include "main.h"
-
 /**
  * _isupper - Entry point
  * Description: checks for uppercase characters
  * @c: carrier integer variable
  * Return: 1 on success, 0 otherwise
  */
-
 int _isupper(int c)
-{
-	char uppercase = "A";
-	int  isupper = 0;
-
-	for (; uppercase <= 'Z'; uppercase++)
 	{
-		if (c == upercase)
+		if (c >= 65 && c <= 90)
 		{
-			isupper = i;
-			break;
+			return (1);
+		}
+		else
+		{
+			return (0);
 		}
 	}
-	return (isupper);
-}
-
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,4 +9,4 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-int _isupper(int c)
+int _isupper(int c);
