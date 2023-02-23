78ee1377679c19bd558d7ea651233d6faed05cbc
MercyBii <mercybii95@gmail.com>
2023-02-23 15:11:11 -0800

    ghjm

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,27 +1,26 @@
 #include "main.h"
-
 /**
  * print_square - Entry point
  * Description: prints a square
  * @size: Carrier integer variable
  * Return: void
  */
-
 void print_square(int size)
 {
 	int i, j;
-
 	if (size <= 0)
 	{
 		_putchar('\n');
 		return;
+
 	}
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size; j++)
+		for (j - 0; j < size; j++)
 		{
 			_putchar('#');
 		}
-		_putchar('\n');
+		putchar('\n');
 	}
 }
+
