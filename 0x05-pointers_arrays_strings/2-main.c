dbae08518f95f1bb31d6adf786f2717d8b268495
MercyBii <mercybii95@gmail.com>
2023-02-27 11:13:16 -0800

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-main.c
+++ 0x05-pointers_arrays_strings/2-main.c
@@ -9,10 +9,10 @@
 int main(void)
 {
 	 char *str;
-    int len;
+	 int len;
 
-    str = "My first strlen!";
-    len = _strlen(str);
-    printf("%d\n", len);
-    return (0);
+	 str = "My first strlen!";
+	 len = _strlen(str);
+	 printf("%d\n", len);
+	 return (0);
 }
