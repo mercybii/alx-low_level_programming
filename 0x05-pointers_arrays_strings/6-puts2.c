484a6c1f06e3a400f508dc4bade71db4b4ed725c
MercyBii <mercybii95@gmail.com>
2023-02-27 14:17:31 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,17 +3,14 @@
 void puts2(char *str)
 {
 	int i;
-	for (i = 0; str[i] != '\0'; i += 2)
+
+	for (i = 0; str[i] != '\0'; i++)
 	{
+		if (i % 2 == 0)
+		{
+			putchar(str[i]);
+		}
 		printf("%c", str[i]);
 	}
 	printf("\n");
-}
-
-int main(void)
-{
-	char str[] = "chebet bii!";
-
-	puts2(str);
-	return (0);
 }
