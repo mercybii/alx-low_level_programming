53710d5da3c507e626565543d65a287a9630fbd9
MercyBii <mercybii95@gmail.com>
2023-02-27 14:22:31 -0800

    6-puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+ /**
+ *  puts2 - Entry point
+ * Description:  prints every other character of a string
+ * starting with the first character
+ * @str: String to be printed
+ * Return: void
+ */
 void puts2(char *str)
 {
 	int i;
