d96dd59e494d78fa6f8927054e2b9c7b27706bf9
MercyBii <mercybii95@gmail.com>
2023-02-27 11:10:29 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen -returns the length of string.
+ * @s: char to check
+ * return 0;
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	for (; *s != '\0'; s++)
+	{
+		c++;
+	}
+	return (c);
+
+}
