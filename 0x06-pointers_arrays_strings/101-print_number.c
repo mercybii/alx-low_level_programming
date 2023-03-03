be8160c2ee0b9714aafd4824ed617d78d3a59a85
MercyBii <mercybii95@gmail.com>
2023-03-02 16:22:05 -0800

    ggnn

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,26 +1,24 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_number - print numbers chars
- * @n: integer
- * Return: 0
+ * print_number - Entry point
+ * Description: Prints an integer
+ * @n: The integer to be printed
+ * Return: Always 0
  */
 
 void print_number(int n)
 {
-	unsigned int x;
-
-	x = n;
+	unsigned int num = n;
 
 	if (n < 0)
 	{
-		_putchar('-');
-		x = -n;
+		putchar('-');
+		num = -num;
 	}
+	if (num > 9)
+		print_number(num / 10);
 
-	if (x / 10 != 0)
-	{
-		print_number(x / 10);
-	}
-	_putchar((x % 10) + '0');
+	putchar((num % 10) + '0');
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,3 +7,4 @@ char *string_toupper(char *);
 char *cap_string(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_number(int n);
