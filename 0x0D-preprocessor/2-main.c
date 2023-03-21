40440b3bf680226bf4fb30b34617cb1853accdb6
mercybii <mercybii95@gmail.com>
2023-03-21 20:47:35 +0300

    3function_like

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints out of the file it was complied from
+ *
+ * Return: Always 0 (success)
+ *
+ * */
+
+int main(void)
+{
+	printf("%s\n", FILE);
+	return(0);
+}
