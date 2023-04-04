33767a549583f6d283eecbbb03664694c92888be
mercybii <mercybii95@gmail.com>
2023-04-04 17:20:51 +0300

    3-add

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -14,4 +14,39 @@
  * Return: A new added node to the list
  */
 
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *last;
+	listint_t *x;
+
+		if (head == NULL)
+		{
+			return (NULL);
+		}
+	last = malloc(sizeof(listint_t));
+	if (last == NULL)
+	{
+		return (NULL);
+	}
+
+	last->n = n;
+	last->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = last;
+		return (last);
+	}
+	else
+	{
+		x = *head;
+		while (x->next != NULL)
+		{
+			x = x->next;
+		}
+		x->next = last;
+	}
+	return (last);
 }
+
+
