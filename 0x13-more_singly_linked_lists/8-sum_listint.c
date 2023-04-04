7c9635fc508a9e6542f52181bd722b5563609cd2
mercybii <mercybii95@gmail.com>
2023-04-05 00:21:19 +0300

    add

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,35 +1,32 @@
 #include <stdio.h>
-#include "lists.h"
 #include <stdlib.h>
+#include"lists.h"
 
 /**
- * get_nodeint_at_index - return the nth node of a listint_t
- *
- * @head: fist node
- *
+ * get_nodeint_at_index - returns the nth node in a list
+ * @head: first node
  * @index: index of the list
- *
- * return
- *
- *
+ * Return: nth node
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int 1 = 0, flag;
 
-	while (head)
+	listint_t *checker_node = head;
+	unsigned int counter = 0;
+
+
+	while (checker_node != NULL)
 	{
-		i++;
-		head =head->next;
-		if (i == index)
-		{
-			flag = 1;
-			return (head);
-		}
+		if  (counter == index)
+			return (checker_node);
+
+		else
+			counter++;
+		checker_node = checker_node->next;
+
 	}
-	if (flag != 1)
-	{
-		return (NULL);
-	}
+
+
+	return (NULL);
 }
