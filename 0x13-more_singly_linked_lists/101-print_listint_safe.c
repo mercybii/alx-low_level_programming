4a39054456bd95f8e6134ab174b6bf08bc7ed375
mercybii <mercybii95@gmail.com>
2023-04-06 02:41:20 +0300

    print

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,38 +3,36 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - prints out a linked list
- *
+ * print_listint_safe - this cond prints out a linked list
  * @head: first node
- *
- * Return: number of nodes
+ * Return: number of nodes or status 98 if fail
  */
 
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t counter = 0;
 	const listint_t *x = head;
-	const listint_t *i = head;
+	const listint_t *y = head;
 
 	if (head == NULL)
 		exit(98);
 
-	while (x != NULL && i != NULL && i->next != NULL)
+	while (x != NULL && y != NULL && y->next != NULL)
 	{
 		printf("[%p] %d\n", (void *)x, x->n);
 
 		x = x->next;
-		i = i->next->next;
+		y = y->next->next;
 
-		if (x == i)
+		if (x == y)
 		{
 			x = head;
-		while (x != i)
+		while (x != y)
 		{
 			printf("[%p] %d\n", (void *)x, x->n);
 
 			x = x->next;
-			i = i->next;
+			y = y->next;
 
 		}
 
