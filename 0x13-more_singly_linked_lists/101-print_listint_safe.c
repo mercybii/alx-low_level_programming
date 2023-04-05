d6d767449b8136a2e236eb3cbb5dcf9e4a0d95fc
mercybii <mercybii95@gmail.com>
2023-04-06 02:03:30 +0300

    print

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,54 +1,88 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
+
 /**
- * print_listint_safe - prints a listint_t linked list
+ * listint_count - Counts the number of unique node
+ * @h: A pointer to the head of the listint_t
  *
- * @head: first node
- *
- * Return: number of nodes
+ * Return: If the list is not looped
  */
-
-size_t print_listint_safe(const listint_t *head)
+size_t listint_count(listint_t *h)
 {
-	size_t counter = 0;
-	const listint_t *i = head;
-	const listint_t *y = head;
+	listint_t *x, *s;
+	size_t nodes = 1;
 
-	if (head == NULL)
-		exit(98);
+	if (h == NULL || h->next == NULL)
+		return (0);
 
-	while (i != NULL && y != NULL && y->next != NULL)
+	x = h->next;
+	s = (h->next)->next;
+
+	while (s)
 	{
-		printf("[%p] %d\n", (void *)i, i->n);
-
-		i = i->next;
-		y = y->next->next;
-
-		if (i == y)
+		if (x == s)
 		{
-			i = head;
-		while (i != y)
-		{
-			printf("[%p] %d\n", (void *)i, i->n);
+			x = h;
+			while (x != s)
+			{
+				nodes++;
+				x = x->next;
+				s = s->next;
+			}
 
-			i = i->next;
-			y = y->next;
+			x = x->next;
+			while (x != s)
+			{
+				nodes++;
+				x = x->next;
+			}
 
+			return (nodes);
 		}
 
-		exit(98);
-		}
-		counter++;
+		x = x->next;
+		s = (s->next)->next;
 	}
-	while (head != NULL)
+
+	return (0);
+}
+
+/**
+ * free_listint_safe - Frees a listint_t
+ * @h: A pointer to the head node
+ *
+ * Return: The size of the list that was freed
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *current;
+	size_t node, i;
+
+	node = listint_count(*h);
+
+	if (node == 0)
 	{
-		printf("[%p] %d\n", (void *)head, head->n);
-
-		head = head->next;
-		counter++;
+		for (; h != NULL && *h != NULL; node++)
+		{
+			current = (*h)->next;
+			free(*h);
+			*h = current;
+		}
 	}
 
-	return (counter);
+	else
+	{
+		for (i = 0; i < node; i++)
+		{
+			current = (*h)->next;
+			free(*h);
+			*h = current;
+		}
+
+		*h = NULL;
+	}
+
+	h = NULL;
+
+	return (node);
 }
