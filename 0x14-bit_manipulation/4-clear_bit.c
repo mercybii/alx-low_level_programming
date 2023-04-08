854b481521adec4aee4f84436c37733e72cd43af
mercybii <mercybii95@gmail.com>
2023-04-08 07:52:40 +0300

    clear_bit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * clear_bit - set the value  of a bit to 0 at the index
+ *
+ * @n: unsigned long int to be tested
+ * @index: start of  unsigntd int
+ *
+ * Return: 1 if it works, -1 if an error occurred
+ *
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+	if ((*n & (1 << index)))
+	{
+		*n = *n | (1 << index);
+	}
+	return (1);
+}
