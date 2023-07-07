af9a4d8becab6ad9761a49eb4dc35d3419854140
mercybii <mercybii95@gmail.com>
2023-07-07 15:49:12 +0300

    1-djb2.c

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,20 @@
+/**
+ * hash_djb2 - calculate the DJBE hash value
+ *
+ * @str: the input to be hashed
+ *
+ * Return: the calculated has value
+ */
+
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+    unsigned long int hash = 5381;
+    int c;
+
+    while ((c = *str++))
+    {
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+    }
+    return (hash);
+}
